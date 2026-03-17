519b07eadabcc8602d2bc0a78e11dd62a586d796
Soom28 <soomin1606@gmail.com>
2026-03-17 12:11:59 +0900

    first commit

---
+++ WEEK3/p97_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    // for(int i = 1; i <= 5; ++i;)
+    for(int i = 5; i >= 1; --i)
+    {
+        // std::cout << 1; << "\n"; 
+        // std::cout << 1 << "\n"; 
+        
+        for(int j = 5; j >= i; --j)
+        {
+            std::cout << j;
+        }
+            std::cout << "\n";
+    }
+}
